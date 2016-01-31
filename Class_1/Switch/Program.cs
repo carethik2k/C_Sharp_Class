d840044f8390ec9459085b18dc9aed76eddd3dd5
Karthik <meetkarthik@gmail.com>
2016-01-30 20:13:03 -0600

    Added the 'Switch' example to Class 1.

---
+++ Class_1/Switch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an color from Red/Green/Blue: ");
+            string inputColor = Console.ReadLine();
+
+            switch(inputColor)
+            {
+                case "Red":
+                    Console.WriteLine("You selected Red");
+                    break;
+
+                case "Green":
+                    Console.WriteLine("You selected Green");
+                    break;
+
+                case "Blue":
+                    Console.WriteLine("You selected Blue");
+                    break;
+
+                default:
+                    Console.WriteLine("Your selection didn't match the standard colors");
+                    break;
+            }
+        }
+    }
+}
