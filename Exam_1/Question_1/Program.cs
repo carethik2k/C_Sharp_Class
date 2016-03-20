f7c2260c9ed4dc12e30ca3f41659820f9ad13200
Manjusri <meetmanjusri@gmail.com>
2016-03-20 14:41:51 -0500

    Exam - Question 1

---
+++ Exam_1/Question_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Question_1
+{
+    class Demo
+    {
+        public Demo()
+        {
+            _testVar = 120;
+        }
+
+        public Demo(int testValue)
+        {
+            _testVar = testValue;
+        }
+
+        private int _testVar;
+
+        public int TestVar
+        {
+            get { return _testVar; }
+            set { _testVar = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo objDemo = new Demo();
+            Console.WriteLine(objDemo.TestVar);
+        }
+    }
+}
